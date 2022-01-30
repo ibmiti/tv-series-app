2363a96aac8f421d6701e48f955caa65264d2054
Dimitri M <ibmiti@gmail.com>
2022-01-29 21:11:38 -0500

    created intro component placed within the app component and returned to the index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const getCurrentDate = () => { 
-  const date = new Date(); // Date class{} doesnt need imported
-  return date.toDateString(); // returns latest date
-}
 
-const greeting = <h1>Hello world, the current date: {getCurrentDate()}</h1>; // using jsx
-// const greeting2 = React.createElement('h2', {}, 'hellow world from h2'); // using js 
-ReactDOM.render(greeting, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
 reportWebVitals(); // performance measurements
 
