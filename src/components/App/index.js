d28300dca0f35a62961c155ef1451b0ae34c0b8f
Dimitri M <ibmiti@gmail.com>
2022-01-30 08:24:58 -0500

    separated components into : src/components/App && src/components/Intro

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
+import Intro from '../Intro';
 import './App.css';
 
-const Intro = (props) => {
-  return (
-    <p className="App-intro">
-      Our first functional Component
-    </p>
-  );
-}
-
 class App extends Component {
+  state = {
+    series : []  // stateObject
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">TV Series List</h1>
         </header>
-        <Intro/>
+        <Intro message="Here you can find all of your most loved series"/>  
+        The length of series array - {this.state.series.length}
       </div>
     );
   }
---
+++ src/components/Intro/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Intro = props => ( // intro component, using shortHandSyntax
+    <p className="App-intro">
+        Our first functional component
+    </p>
+);
+
+export default Intro;
