fe3a3e01d6a54847dd701297736934671e374c40
Dimitri M <ibmiti@gmail.com>
2022-01-30 14:41:46 -0500

    will use fetchAPI to grab the data from endpoint.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class App extends Component {
   state = {
-    series : []  // stateObject
+    series : []  // stateObject : is set by componentDidMount()'s return
   }
 
   // component lifecycle management;
