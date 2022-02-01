832653912766891790743f5be14c6624ca8f2f01
Dimitri M <ibmiti@gmail.com>
2022-02-01 08:23:59 -0500

    adding loader component

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 import SeriesList from '../../components/SeriesList';
+import Loader from '../../components/Loader';
 
 class Series extends Component {
 
     state = {
-        series : []
+        series : [],
+        seriesName: '',
+        isFetching: false
     }
 
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-    .then(response => response.json())   
-    .then(json => this.setState({ series : json }));
-  }
+    onSeriesInputChange = e => { 
+        this.setState({ seriesName: e.target.value, isFetching: true });
+
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
+        .then(response => response.json())   
+        .then(json => this.setState({ series : json }));
+    }
+
 
     render() {
+        const { series, seriesName, isFetching } = this.state;
+
         return (
             <div>
-                The length of series array - {this.state.series.length}
-                <SeriesList list={this.state.series} />
+                <div>
+                    <input 
+                        value={seriesName}
+                        type='text' 
+                        onChange={this.onSeriesInputChange} />
+                </div>
+                {
+                   !isFetching && series.length === 0 && seriesName.trim() === '' 
+                    &&
+                    <p>Please enter series name into the input</p>
+                }
+                {
+                    !isFetching && series.length === 0 && seriesName.trim !== ''
+                    &&
+                    <p>No TV series have been found with this name</p>
+                }
+                {
+                    isFetching && <Loader/>
+                }
+                {
+                    !isFetching && <SeriesList list={this.state.series} />
+                }
             </div>
         )
     }
-
 }
-
+        
 export default Series;
